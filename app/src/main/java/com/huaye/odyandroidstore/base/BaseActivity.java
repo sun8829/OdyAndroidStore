87de45af314db1148783a5075e05763b26cacccd
Samuel <753018829@qq.com>
2017-04-07 14:50:00 +0800

    去除冗余代码

--- app/src/main/java/com/huaye/odyandroidstore/base/BaseActivity.java
+++ app/src/main/java/com/huaye/odyandroidstore/base/BaseActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.Collections;
-
 /**
  * Created by sunhuahui on 2017/1/28.
  */
@@ -58,8 +56,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
         initData();
         initListener();
-
-        Collections.synchronizedMap()
     }
 
 }
