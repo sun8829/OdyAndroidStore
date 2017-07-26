4a134d23293e28e50b6f0aa2124900b386ce2bcf
sunhuahui <sunhuahui@ppdai.com>
2017-07-26 11:12:37 +0800

    增加线程死锁示例

--- app/src/test/java/com/ody/study/ThreadTest.java
+++ app/src/test/java/com/ody/study/ThreadTest.java
@@ -48,6 +48,26 @@ public class ThreadTest {
         sleep();
     }
 
+    @Test
+    public void threadDeadLockTest()  throws InterruptedException {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        Object obj3 = new Object();
+
+        Thread t1 = new Thread(new SyncThread(obj1, obj2), "t1");
+        Thread t2 = new Thread(new SyncThread(obj2, obj3), "t2");
+        Thread t3 = new Thread(new SyncThread(obj3, obj1), "t3");
+
+        t1.start();
+        Thread.sleep(5000);
+        t2.start();
+        Thread.sleep(5000);
+        t3.start();
+
+
+        sleep();
+    }
+
 
     private void sleep() {
         try {
@@ -183,6 +203,42 @@ public class ThreadTest {
                     e.printStackTrace();
                 }
                 System.out.println("" + this.getName() + "-----" + i);
+            }
+        }
+    }
+
+    class SyncThread implements Runnable {
+        private Object obj1;
+        private Object obj2;
+
+        public SyncThread(Object o1, Object o2) {
+            this.obj1 = o1;
+            this.obj2 = o2;
+        }
+
+        @Override
+        public void run() {
+            String name = Thread.currentThread().getName();
+            System.out.println(name + " acquiring lock on " + obj1);
+            synchronized (obj1) {
+                System.out.println(name + " acquired lock on " + obj1);
+                work();
+                System.out.println(name + " acquiring lock on " + obj2);
+                synchronized (obj2) {
+                    System.out.println(name + " acquired lock on " + obj2);
+                    work();
+                }
+                System.out.println(name + " released lock on " + obj2);
+            }
+            System.out.println(name + " released lock on " + obj1);
+            System.out.println(name + " finished execution.");
+        }
+
+        private void work() {
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
