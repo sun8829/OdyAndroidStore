7c8ce279c9561f70f4fde7bbf423aa604c9df697
sunhuahui <sunhuahui@ppdai.com>
2017-07-28 18:25:32 +0800

    优化切换

--- app/src/main/java/com/huaye/odyandroidstore/MainActivity.java
+++ app/src/main/java/com/huaye/odyandroidstore/MainActivity.java
@@ -1,10 +1,14 @@
 package com.huaye.odyandroidstore;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.Postcard;
 import com.alibaba.android.arouter.facade.callback.NavCallback;
@@ -14,11 +18,14 @@ import com.huaye.odyandroidstore.library.LibraryFragment;
 import com.huaye.odyandroidstore.my.MyFragment;
 import com.huaye.odyandroidstore.welfare.WelfareFragment;
 
+import java.util.List;
+
 import cn.bmob.v3.Bmob;
 
 public class MainActivity extends BaseActivity {
     private LibraryFragment libraryFragment;
     private WelfareFragment welfareFragment;
+    private MyFragment myFragment;
     private FragmentManager fm;
     private BottomNavigationView navigationView;
 
@@ -38,7 +45,25 @@ public class MainActivity extends BaseActivity {
     protected void initView() {
         navigationView = (BottomNavigationView) findViewById(R.id.nav);
         libraryFragment = new LibraryFragment();
-        fm.beginTransaction().replace(R.id.container, libraryFragment).commitAllowingStateLoss();
+        if (libraryFragment.isAdded()) {
+            fm.beginTransaction().show(libraryFragment).commit();
+        } else {
+            fm.beginTransaction().add(R.id.container, libraryFragment).commit();
+        }
+    }
+
+    private void switchFragment(Fragment fragment) {
+        List<Fragment> fragments = fm.getFragments();
+        for (Fragment f : fragments) {
+            if (!f.equals(fragment) && !f.isHidden()) {
+                fm.beginTransaction().hide(f).commit();
+            }
+        }
+        if (fragment.isAdded()) {
+            fm.beginTransaction().show(fragment).commit();
+        } else {
+            fm.beginTransaction().add(R.id.container, fragment).commit();
+        }
     }
 
     @Override
@@ -49,13 +74,19 @@ public class MainActivity extends BaseActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.menu_library:
-                        fm.beginTransaction().replace(R.id.container, libraryFragment).commitAllowingStateLoss();
+                        switchFragment(libraryFragment);
                         break;
                     case R.id.menu_welfare:
-                        fm.beginTransaction().replace(R.id.container, new WelfareFragment()).commitAllowingStateLoss();
+                        if (welfareFragment == null){
+                            welfareFragment = new WelfareFragment();
+                        }
+                        switchFragment(welfareFragment);
                         break;
                     case R.id.menu_my:
-                        fm.beginTransaction().replace(R.id.container, new MyFragment()).commitAllowingStateLoss();
+                        if (myFragment == null){
+                            myFragment = new MyFragment();
+                        }
+                        switchFragment(myFragment);
                         break;
                 }
                 return true;
@@ -83,5 +114,72 @@ public class MainActivity extends BaseActivity {
                 Log.d("ARouter", "被拦截了");
             }
         });
+
+        getSupportFragmentManager().registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
+            @Override
+            public void onFragmentPreAttached(FragmentManager fm, Fragment f, Context context) {
+                super.onFragmentPreAttached(fm, f, context);
+            }
+
+            @Override
+            public void onFragmentAttached(FragmentManager fm, Fragment f, Context context) {
+                super.onFragmentAttached(fm, f, context);
+            }
+
+            @Override
+            public void onFragmentCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
+                super.onFragmentCreated(fm, f, savedInstanceState);
+            }
+
+            @Override
+            public void onFragmentActivityCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
+                super.onFragmentActivityCreated(fm, f, savedInstanceState);
+            }
+
+            @Override
+            public void onFragmentViewCreated(FragmentManager fm, Fragment f, View v, Bundle savedInstanceState) {
+                super.onFragmentViewCreated(fm, f, v, savedInstanceState);
+            }
+
+            @Override
+            public void onFragmentStarted(FragmentManager fm, Fragment f) {
+                super.onFragmentStarted(fm, f);
+            }
+
+            @Override
+            public void onFragmentResumed(FragmentManager fm, Fragment f) {
+                super.onFragmentResumed(fm, f);
+            }
+
+            @Override
+            public void onFragmentPaused(FragmentManager fm, Fragment f) {
+                super.onFragmentPaused(fm, f);
+            }
+
+            @Override
+            public void onFragmentStopped(FragmentManager fm, Fragment f) {
+                super.onFragmentStopped(fm, f);
+            }
+
+            @Override
+            public void onFragmentSaveInstanceState(FragmentManager fm, Fragment f, Bundle outState) {
+                super.onFragmentSaveInstanceState(fm, f, outState);
+            }
+
+            @Override
+            public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
+                super.onFragmentViewDestroyed(fm, f);
+            }
+
+            @Override
+            public void onFragmentDestroyed(FragmentManager fm, Fragment f) {
+                super.onFragmentDestroyed(fm, f);
+            }
+
+            @Override
+            public void onFragmentDetached(FragmentManager fm, Fragment f) {
+                super.onFragmentDetached(fm, f);
+            }
+        }, true);
     }
 }
