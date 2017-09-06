1eb93b974570a8c6114dd561c08bfb83e4353616
sunhuahui <sunhuahui@ppdai.com>
2017-09-06 17:54:42 +0800

    分支测试

--- app/src/test/java/com/ody/study/GsonTest.java
+++ app/src/test/java/com/ody/study/GsonTest.java
@@ -2,14 +2,19 @@ package com.ody.study;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.ody.study.util.LoggerFactory;
 
 import org.junit.Test;
+
+import static android.R.attr.data;
 
 /**
  * Created by Samuel on 2017/6/9.
  */
 
 public class GsonTest {
+    private LoggerFactory logger = LoggerFactory.INSTANCE;
+
     @Test
     public void emptyObjTest() {
         String data = "{\n" +
@@ -29,7 +34,18 @@ public class GsonTest {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
     }
+
+    @Test
+    public void convertTest() {
+        String data = "{'price':'16.23630', 'author': '孙华辉'}";
+        Bean bean = new Gson().fromJson(data, Bean.class);
+        logger.info("price {}", bean.price * 100);
+    }
+
+    public static class Bean {
+        public float price;
+        public String author;
+    }
+
 }
